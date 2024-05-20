dc6c31e442fa7a905937a46f68b61be0fe9ea5cb
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-05-20 07:45:53 +0700

    set up tailwind

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,17 +1,7 @@
-import Counter from "../_components/Counter";
-
 export default async function Page() {
-	const res = await fetch(`https://jsonplaceholder.typicode.com/comments`);
-	const data = await res.json();
 	return (
 		<div>
 			<h1>Cabins Page</h1>
-			<ul>
-				{data.map((comment) => (
-					<li key={comment.id}>{comment.name}</li>
-				))}
-			</ul>
-			<Counter />
 		</div>
 	);
 }

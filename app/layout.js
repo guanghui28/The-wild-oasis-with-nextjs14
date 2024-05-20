0997e62549edaf8966021400c3ac0a365540fd0f
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-05-20 08:39:58 +0700

    style root layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
 	subsets: ["latin", "vietnamese"],
@@ -21,14 +20,12 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body
-				className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+				className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
 			>
-				<header>
-					<Logo />
-				</header>
-				<Navigation />
-				<main>{children}</main>
-				<footer>Copyright by The Wild Oasis</footer>
+				<Header />
+				<div className="flex-1 px-8 py-12">
+					<main className="max-w-7xl mx-auto bg-red-500 ">{children}</main>
+				</div>
 			</body>
 		</html>
 	);

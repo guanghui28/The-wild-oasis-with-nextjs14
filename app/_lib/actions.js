3f707458e56a8a0ff26842df910389336b8c957b
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-05-22 13:37:38 +0700

    update profile

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,10 +1,30 @@
 "use server";
 
-import { signIn, signOut } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
+import { supabase } from "./supabase";
 
 export async function updateProfile(formData) {
-	console.log(formData);
-	console.log("Server action");
+	const session = await auth();
+	console.log(session);
+	if (!session) throw new Error("You must be logged in first!");
+	const nationalID = formData.get("nationalID");
+	const [nationality, countryFlag] = formData.get("nationality").split("%");
+	const regex = /^[a-zA-z0-9]{6,12}$/;
+	if (!regex.test(nationalID))
+		throw new Error("Please provided valid nationalID");
+
+	const updateData = {
+		nationalID,
+		countryFlag,
+		nationality,
+	};
+
+	const { data, error } = await supabase
+		.from("guests")
+		.update(updateData)
+		.eq("id", session.user.guestId);
+
+	if (error) throw new Error("Guest could not be updated");
 }
 
 export async function signInAction() {

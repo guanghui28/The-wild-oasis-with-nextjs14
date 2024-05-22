aae8972edf6bf17659d85c9707d6b652af8ee5fe
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-05-22 14:52:12 +0700

    delete booking

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { auth, signIn, signOut } from "@/auth";
 import { supabase } from "./supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "./data-service";
 
 export async function updateProfile(formData) {
 	const session = await auth();
@@ -27,6 +28,24 @@ export async function updateProfile(formData) {
 	if (error) throw new Error("Guest could not be updated");
 
 	revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+	const session = await auth();
+	if (!session) throw new Error("You must be logged in first!");
+
+	const guestBookings = await getBookings(session.user.guestId);
+	const guestBookingIds = guestBookings.map((booking) => booking.id);
+	if (!guestBookingIds.includes(bookingId))
+		throw new Error("You are not allowed delete this booking!");
+
+	const { error } = await supabase
+		.from("bookings")
+		.delete()
+		.eq("id", bookingId);
+	if (error) throw new Error("Booking couldn't be deleted!");
+
+	revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {

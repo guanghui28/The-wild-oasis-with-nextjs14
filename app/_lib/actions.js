0a9d24586ab3620fa4523d77a46d867af6b6cedc
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-05-22 13:43:39 +0700

    revalidate cached

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,6 +2,7 @@
 
 import { auth, signIn, signOut } from "@/auth";
 import { supabase } from "./supabase";
+import { revalidatePath } from "next/cache";
 
 export async function updateProfile(formData) {
 	const session = await auth();
@@ -25,6 +26,8 @@ export async function updateProfile(formData) {
 		.eq("id", session.user.guestId);
 
 	if (error) throw new Error("Guest could not be updated");
+
+	revalidatePath("/account/profile");
 }
 
 export async function signInAction() {

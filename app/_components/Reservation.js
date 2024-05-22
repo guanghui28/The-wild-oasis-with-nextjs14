8e4a3875b9b185ca23dd1d4060908bb81aa02907
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-05-22 19:10:24 +0700

    dealing with selected range dates

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -13,7 +13,11 @@ export default async function Reservation({ cabin }) {
 
 	return (
 		<div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
-			<DateSelector settings={settings} bookDates={bookedDates} cabin={cabin} />
+			<DateSelector
+				settings={settings}
+				bookedDates={bookedDates}
+				cabin={cabin}
+			/>
 			{session?.user ? (
 				<ReservationForm cabin={cabin} user={session.user} />
 			) : (
